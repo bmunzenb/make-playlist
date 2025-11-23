7f1a3d33293d57e4589bc1dc2c818c4e0613dd15
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2025-11-23 17:30:45 -0500

    implement make playlist command

--- app/src/main/kotlin/com/munzenberger/playlist/MakePlaylist.kt
+++ app/src/main/kotlin/com/munzenberger/playlist/MakePlaylist.kt
@@ -2,7 +2,15 @@ package com.munzenberger.playlist
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
 
 class MakePlaylist : CliktCommand() {
     val directory by argument().path(
@@ -10,7 +18,37 @@ class MakePlaylist : CliktCommand() {
         canBeFile = false
     )
 
+    val album by option("--album")
+        .default("Playlist")
+        .help("Album name, written to ALBUM field")
+
+    val albumArtist by option("--album-artist")
+        .default("Various")
+        .help("Album artist, written to ALBUM_ARTIST field")
+
     override fun run() {
-        println("hello world!")
+        val files = Files
+            .newDirectoryStream(directory, "*.mp3")
+            .use { stream ->
+                stream
+                    .sortedBy { Files.readAttributes(it, BasicFileAttributes::class.java).creationTime() }
+                    .toList()
+            }
+
+        files.forEachIndexed { index, file ->
+            process(file, index + 1)
+        }
+    }
+
+    private fun process(file: Path, track: Int) {
+        println("Track $track: ${file.fileName}")
+
+        val f = AudioFileIO.read(file.toFile())
+        f.tag.apply {
+            setField(FieldKey.TRACK, track.toString())
+            setField(FieldKey.ALBUM, album)
+            setField(FieldKey.ALBUM_ARTIST, albumArtist)
+        }
+        f.commit()
     }
 }
